65c84aa59e0ea9bc494de0da05b397ad851669db
Frederik Reher <reher.frederik@gmail.com>
2021-01-29 20:46:30 +0100

    Added basic playercontroller, camera controller

---
+++ Assets/Scripts/Utility/Additions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Additions
+{
+    public static Vector3 To3D(this Vector2 value)
+        => new Vector3(value.x, 0f, value.y);
+
+    public static Vector2 To2D(this Vector3 value)
+        => new Vector2(value.x, value.z);
+}
