5b31c126a51242ee939bb76fd7622ccb3968b951
Frederik Reher <reher.frederik@gmail.com>
2021-01-30 00:41:44 +0100

    Added sliding doors

---
+++ Assets/Scripts/Interaction/Interactive_Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactive_Door : Interactive
+{
+	protected bool isMoving;
+
+	protected bool isOpen;
+
+	public override bool IsInteractive => !isMoving;
+
+	public override void Interact(InteractionComponent trigger)
+	{
+		if(!isOpen)
+		{
+			Open();
+		}
+		else
+		{
+			Close();
+		}
+	}
+
+	protected abstract void Open();
+
+	protected abstract void Close();
+}
---
+++ Assets/Scripts/Interaction/Interactive_SlidingDoor.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class Interactive_SlidingDoor : Interactive_Door
+{
+	[SerializeField]
+	private Transform doorObject;
+
+	private Vector3 closedPos = default;
+
+	[SerializeField]
+	private Vector3 offset = default;
+
+	[SerializeField, Range(0.1f, 15f)]
+	private float moveDuration = 1f;
+
+	private float current = 0f;
+
+	private new Collider collider;
+
+	private void Start()
+	{
+		if(!doorObject)
+		{
+			enabled = false;
+			return;
+		}
+		collider = doorObject.GetComponent<Collider>();
+		closedPos = doorObject.transform.position;
+	}
+
+	void Update()
+	{
+		if (!isMoving)
+			return;
+
+		var offset = doorObject.transform.rotation * this.offset;
+
+		var startPosition = isOpen ? closedPos + offset : closedPos;
+		var endPosition = isOpen ? closedPos : closedPos + offset;
+
+		current += Time.smoothDeltaTime;
+
+		doorObject.transform.position = Vector3.Slerp(startPosition, endPosition, current / moveDuration);
+		
+		if(current >= moveDuration)
+		{
+			isMoving = false;
+			isOpen = !isOpen;
+			collider.enabled = !isOpen;
+		}
+	}
+
+	protected override void Close()
+	{
+		isMoving = true;
+		current = 0f;
+		collider.enabled = true;
+	}
+
+	protected override void Open()
+	{
+		isMoving = true;
+		current = 0f;
+	}
+
+
+}
