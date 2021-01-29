bb364f8cef79daefa4b8004ee222fb615fd56f4a
Felix <fefix.dahmen@gmx.de>
2021-01-29 21:59:04 +0100

    the enemy Rotate now to the target

--- Assets/Scripts/KI/KIController.cs
+++ Assets/Scripts/KI/KIController.cs
@@ -6,12 +6,12 @@ public class KIController : MonoBehaviour
 {
     public List<Transform> Waypoints = new List<Transform>();
     private int index = 0;
-    public float range = 0.0f;
+    public float range = 1.0f;
     private Vector3 currentGoalPosition;
 
     private CharacterController charContr;
 
-    public float MovementSpeed = 0.0f;
+    public float MovementSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,17 +32,22 @@ public class KIController : MonoBehaviour
 
     public void Move(Vector3 _goal)
     {
-        charContr.Move((MoveDirection(_goal) * MovementSpeed * Time.deltaTime));
+        transform.LookAt(new Vector3(_goal.x, 0, _goal.z));
+        Vector3 dir = MoveDirection(_goal);
+        dir *= MovementSpeed * Time.deltaTime;
+        dir = transform.TransformDirection(dir);
+
+        charContr.Move(dir);
     }
 
     public Vector3 MoveDirection(Vector3 _goal)
     {
-        return -(transform.position - _goal).normalized;
+        return transform.InverseTransformDirection(-(transform.position - _goal).normalized);
     }
 
     public bool AtGoal(Vector3 _goal)
     {
-        return Vector3.Distance(transform.position, _goal) < range;
+        return Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(_goal.x, 0, _goal.z)) < range;
     }
 
     public void NextWaypoint()
