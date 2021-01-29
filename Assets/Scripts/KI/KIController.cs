141eaf94ffcd9412961804f46fa1840563037fee
Felix <fefix.dahmen@gmx.de>
2021-01-29 22:54:35 +0100

    Enemy Target Player

--- Assets/Scripts/KI/KIController.cs
+++ Assets/Scripts/KI/KIController.cs
@@ -12,6 +12,12 @@ public class KIController : MonoBehaviour
     private CharacterController charContr;
 
     public float MovementSpeed = 2.0f;
+
+    public Transform Player;
+    public float ViewRange = 4.0f;
+    public float ViewAngle = 45.0f;
+
+    public float RotationSpeed = 20.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +28,11 @@ public class KIController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (PlayerInViewSpace())
+        {
+
+        }
+
         if (AtGoal(currentGoalPosition))
         {
             NextWaypoint();
@@ -32,8 +43,10 @@ public class KIController : MonoBehaviour
 
     public void Move(Vector3 _goal)
     {
-        transform.LookAt(new Vector3(_goal.x, 0, _goal.z));
         Vector3 dir = MoveDirection(_goal);
+        Quaternion toRotation = Quaternion.LookRotation(-(transform.position - _goal));
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, RotationSpeed * Time.deltaTime);
+        //transform.LookAt(new Vector3(_goal.x, 0, _goal.z));
         dir *= MovementSpeed * Time.deltaTime;
         dir = transform.TransformDirection(dir);
 
@@ -61,5 +74,27 @@ public class KIController : MonoBehaviour
             index = 0;
         }
         currentGoalPosition = Waypoints[index].position;
+    }
+
+    public bool PlayerInViewSpace()
+    {
+        if(Vector3.Distance(new Vector3(transform.position.x, 0, transform.position.z), new Vector3(Player.position.x, 0, Player.position.z)) < ViewRange)
+        {
+            if (Vector3.Angle(MoveDirection(Player.position), Vector3.forward) < ViewAngle)
+            {
+                currentGoalPosition = Player.position;
+                return true;
+            }
+            else
+            {
+                currentGoalPosition = Waypoints[index].position;
+                return false;
+            }
+        }
+        else
+        {
+            currentGoalPosition = Waypoints[index].position;
+            return false;
+        }
     }
 }
