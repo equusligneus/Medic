ae5c2fa51294cd35f0d010fa0a438059e4ceeb9e
Frederik Reher <reher.frederik@gmail.com>
2021-01-30 22:45:17 +0100

    GameStartScript, Changes to Hud

--- Assets/Scripts/GameStart/StartGameScript.cs
+++ Assets/Scripts/GameStart/StartGameScript.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public abstract class StartGameTask : ScriptableObject
+{
+	public abstract void Execute(StartGameScript startGameScript, Action onDone);
+}
+
+[RequireComponent(typeof(DungeonRoom))]
 public class StartGameScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private StartGameTask[] startGameTasks = default;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private int index = 0;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (startGameTasks.Length > 0)
+			startGameTasks[0].Execute(this, OnTaskDone);
+	}
+
+	private void OnTaskDone()
+	{
+		++index;
+		if (index < startGameTasks.Length)
+			startGameTasks[index].Execute(this, OnTaskDone);
+	}
 }
