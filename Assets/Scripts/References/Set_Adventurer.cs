286f33f3bbeeeff7c8cbbe3154e8c4d4fde1aca5
Frederik Reher <reher.frederik@gmail.com>
2021-01-30 13:52:17 +0100

    Added dummies for adventurer hud

---
+++ Assets/Scripts/References/Set_Adventurer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Set_Adventurer", menuName = "Medic/Set/Adventurer")]
+public class Set_Adventurer : RuntimeSet<Adventurer>
+{
+}
