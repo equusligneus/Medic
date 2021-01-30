4bd5f16c9007e03d2f13e49847ba9185b4e482ec
Frederik Reher <reher.frederik@gmail.com>
2021-01-30 13:50:38 +0100

    Added HUD for HP
    Also UI system...

---
+++ Assets/Scripts/UI/UI_LifeCount.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class UI_LifeCount : MonoBehaviour
+{
+    [SerializeField]
+    private Ref<int> currentLives = default;
+
+    [SerializeField]
+    private Ref<int> maxLives = default;
+
+    [SerializeField]
+    private VisualTreeAsset lifeIcon = default;
+
+    [SerializeField]
+    private Sprite lifeEmpty = default;
+
+    [SerializeField]
+    private Sprite lifeFull = default;
+
+    private List<VisualElement> lives = new List<VisualElement>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	private void OnEnable()
+	{
+        currentLives.OnChanged += OnLivesChanged;
+        maxLives.OnChanged += OnMaxLivesChanged;
+
+        OnMaxLivesChanged();
+    }
+
+	private void OnDisable()
+	{
+        currentLives.OnChanged -= OnLivesChanged;
+        maxLives.OnChanged -= OnMaxLivesChanged;
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+    private void OnLivesChanged()
+    {
+        for (int i = 0; i < maxLives.Get(); ++i)
+        {
+            var bg = lives[i].style.backgroundImage;
+            var image = bg.value;
+            image.sprite = i < currentLives.Get() ? lifeFull : lifeEmpty;
+            bg.value = image;
+            lives[i].style.backgroundImage = bg;
+        }
+    }
+
+    private void OnMaxLivesChanged()
+	{
+        var doc = GetComponent<UIDocument>();
+        var parent = doc.rootVisualElement.Q<VisualElement>("Lives");
+
+        int childCount = parent.childCount;
+
+        // remove surplus elements
+        for(int i = maxLives.Get(); i < childCount; ++i)
+            parent.RemoveAt(i);
+
+        for (int i = childCount; i < maxLives.Get(); ++i)
+            lifeIcon.CloneTree(parent);
+
+        lives.Clear();
+        // do stuff to lives
+        foreach(var child in parent.Children())
+            lives.Add(child.Q<VisualElement>("Life"));
+
+        OnLivesChanged();
+	}
+}
