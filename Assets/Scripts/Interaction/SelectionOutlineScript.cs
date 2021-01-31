1ae48ebe27757bb96b91650f5eb3761e86e70650
Frederik Reher <reher.frederik@gmail.com>
2021-01-31 12:26:00 +0100

    Added a better outline script

---
+++ Assets/Scripts/Interaction/SelectionOutlineScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Interactive))]
+public class SelectionOutlineScript : MonoBehaviour
+{
+	[SerializeField]
+	private cakeslice.Outline outline = default;
+
+	private void OnEnable()
+	{
+		var trigger = GetComponent<Interactive>();
+		trigger.OnSelected += OnSelection;
+		OnSelection(false);
+	}
+
+	private void OnDisable()
+	{
+		var trigger = GetComponent<Interactive>();
+		trigger.OnSelected -= OnSelection;
+	}
+
+	private void OnSelection(bool showOutline)
+	{
+		outline.enabled = showOutline;
+	}
+}
