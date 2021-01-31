c186bde286efa476d766ef5991afb532f09dce32
b34s7m0d3 <peter@petermehlmann.com>
2021-01-31 10:56:57 +0100

    Music on player, success sound on completing objective

--- Assets/Scripts/Adventurer/RescueZone.cs
+++ Assets/Scripts/Adventurer/RescueZone.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(AudioSource))]
 public class RescueZone : MonoBehaviour
 {
     [SerializeField]
     private ParticleController _DespawnParticle;
+
+    AudioSource audioData;
+
+    private void Awake()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+
+
+
     private void OnTriggerEnter(Collider other)
     {
+        
         Adventurer adv = other.GetComponentInParent<Adventurer>();
         if(adv != null)
         {
             _DespawnParticle.ToggleAllParticles(true, adv.transform.position);
+            audioData.Play(0);
             adv.GotRescued();
         }
     }
